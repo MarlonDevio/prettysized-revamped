451b45dc8d9de527dbf3991c3ce59fad79223165
MarlonHamaekersPXL <mar.hamaekers@gmail.com>
2024-06-07 20:52:16 +0200

    playing with some components

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,30 @@ module.exports = {
     './assets/*.{js,css}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#000000',
+        'accent-light': '#FFAECF',
+        accent: '#F658AD',
+        gray: 'hsl(0, 0%, 80%)',
+        white: '#FCFCFC',
+      },
+      screens: {
+        mobile: '320px',
+        'mobile-xl': '480px',
+        tablet: '768px',
+        laptop: '1024px',
+        'laptop-xl': '1280px',
+        desktop: '1440px',
+      },
+      fontSize: {
+        'header-large': 'calc(1.8rem + var(--vw))',
+        'header-medium': 'calc(1.4rem + var(--vw))',
+        'header-small': 'calc(1.0rem + var(--vw))',
+        'sub-header-large': 'calc(0.6rem + var(--vw))',
+        'sub-header-medium': 'calc(0.2rem + var(--vw))',
+      },
+    },
   },
   plugins: [],
 };
